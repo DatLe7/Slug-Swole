b8bc1f136055154374e6872a0da963e7fbec934e
Koolaidpick <dat90189911@gmail.com>
2024-11-27 15:07:59 -0800

    implemented the data tracking page (currently using dummy data)

--- frontend/lib/data_tracking.dart
+++ frontend/lib/data_tracking.dart
@@ -1,5 +1,7 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
+import 'dart:math';
 
 //Page for graphs and capacity tracking
 class DataPage extends StatelessWidget {
@@ -7,7 +9,10 @@ class DataPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    final now = DateTime.now();
+    String formattedDate = DateFormat.yMMMEd().format(now);
+    final List<FlSpot> dummyData = List.generate(8, (index) { return FlSpot(index.toDouble(), (Random().nextDouble()*120)); });
+    double current = dummyData[dummyData.length - 1].y;
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.secondary,
@@ -24,18 +29,60 @@ class DataPage extends StatelessWidget {
               ),
               child: Column(
                 children: [
-                  Text("Placeholder Date"),
+                  Text(formattedDate),
                   Text("Capacity: "),
-                  Text("xxx / 120"),
+                  Text("${current.toInt()} / 120"),
                 ],
               ),
             ),
           ),
-          SizedBox(height: 20,)
+          SizedBox(height: 20,),
+          Container(
+            height: 400,
+            width: 600,
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.white, 
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: LineChart(
+              LineChartData(
+                maxY: 120,
+                minY: 0,
+                lineTouchData: LineTouchData(enabled: false),
+                borderData: FlBorderData(show: false),
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: dummyData,
+                    isCurved: true,
+                    barWidth: 3,
+                    color: Colors.black
+                  ),
+                ],
+                titlesData: FlTitlesData(
+                  topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  bottomTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 35,
+                      interval: 20,
+                      maxIncluded: true,
+                      minIncluded: true,
+                    ),
+                  ),
+                  rightTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: false,
+                      reservedSize: 35,
+                    )
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
-      
-
     );
   }
 }
