3bded658835d0f073e98caedc9123180715d22d9
tyler-ham05 <tyjham05@gmail.com>
2024-12-16 17:18:03 -0800

    Built listbuilder for splits page, imported like button library

--- frontend/lib/splits.dart
+++ frontend/lib/splits.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:like_button/like_button.dart';
 
 //Page for recording splits
 class SplitsPage extends StatefulWidget {
@@ -9,20 +11,52 @@ class SplitsPage extends StatefulWidget {
 }
 
 class _SplitsPageState extends State<SplitsPage> {
+  List dummySplits = List.generate(20, (index) => {"name":"Split $index", "target":"TEMP","exercises":[],"likes":Random().nextInt(1500),});
   @override
+
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
     return Scaffold(
       backgroundColor: theme.primaryColor,
-      body:Center(
-        child: Column(
-          children: [
-            Text("Page 2"),
-            
-          ],
-        ),
-      ),
+      body: ListView.builder(
+        itemCount: dummySplits.length,
+        itemBuilder: (BuildContext context,int index){ 
+          return Center(
+            child: GestureDetector(
+              onTap: (){
+                print('Tapped on item $index');
+              },
+              child:Column(
+                children: [
+                  SizedBox(height:20),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    width: MediaQuery.sizeOf(context).width * 0.4,
+                    decoration: BoxDecoration(
+                    color: Colors.white, 
+                    borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Text("Name: ${dummySplits[index]["name"]}"),
+                        Text("Target: ${dummySplits[index]["target"]}"),
+                        Text("Likes: ${dummySplits[index]["likes"].toString()}"),
+                        LikeButton(
+                          onTap: (bool isLiked) async{
+                          return !isLiked;
+                          },
+                        )
+                      ],
+                    )
+                              
+                  ),
+
+                ],
+              )
+            ),
+          );
+      }),
 
     );
   }
