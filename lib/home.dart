a3f356827ced26fcf7c2ce604b65f45ca1c76d1f
tyler-ham05 <tyjham05@gmail.com>
2025-04-09 22:55:25 -0700

    cleaning up messes

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:slug_swole/backend_services.dart';
+//import 'package:slug_swole/backend_services.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'data_tracking.dart';
 
@@ -12,30 +12,9 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   var selectedIndex = 0;
   //calls from backend_services.dart to check if user is an admin
-  Future data = adminAuth();
 
   @override
   Widget build(BuildContext context) {
-    void newState(int index) {
-      setState(() {
-        selectedIndex = index;
-      });
-    }
-
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = DataPage();
-        break;
-      /*
-      case 1:
-        page = SplitsPage();
-        break;
-      */
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
@@ -86,41 +65,8 @@ class HomePageState extends State<HomePage> {
             ],
             centerTitle: true,
           ),
-          body: page,
-          bottomNavigationBar: FutureBuilder(
-              future: data,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return SizedBox.shrink();
-                }
-                if (snapshot.hasError) {
-                  return SizedBox.shrink();
-                }
-                if (!snapshot.hasData || snapshot.data == null) {
-                  return SizedBox.shrink();
-                }
-                if(snapshot.data){
-                return BottomNavigationBar(
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.bar_chart_outlined),
-                      label: 'Home',
-                    ),
-                    /*BottomNavigationBarItem(
-                        icon: Icon(Icons.fitness_center),
-                        label: "Splits",
-                      ),*/
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.shield_outlined),
-                      label: "counter",
-                    ),
-                  ],
-                  currentIndex: selectedIndex,
-                  onTap: newState,
-                );
-                }
-               else {return SizedBox.shrink();} 
-              }),
+          body: DataPage(),
+
         );
       },
     );
